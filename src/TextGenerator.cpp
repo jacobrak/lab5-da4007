13cb3491b926470842604d6244c5f3e17a3d7f4c
jacobrak <155147415+jacobrak@users.noreply.github.com>
2025-10-29 10:11:25 +0100

    Added. Calling Language model inside TextGenerator

--- src/TextGenerator.cpp
+++ src/TextGenerator.cpp
@@ -1,11 +1,23 @@
 #include "TextGenerator.h"
+#include "LanguageModel.h"
 #include <iostream>
 
 using namespace std;
-TextGenerator::TextGenerator(int k_input, int length_input){
+
+TextGenerator::TextGenerator(int k_input, int length_input, const string& filename){
     k = k_input;
     length = length_input;
+    text_file = filename;
 }
+
+string TextGenerator::Generate_text(){
+    LanguageModel Model(k);
+    Model.train(text_file);
+    string random_text = Model.PickRandom();
+
+    return random_text;
+}
+
 string TextGenerator::ReturnWord(){
     return word;
 }
--- src/TextGenerator.h
+++ src/TextGenerator.h
@@ -1,14 +1,20 @@
+#ifndef TEXTGENERATOR_H
+#define TEXTGENERATOR_H
 #include <iostream>
 
 using namespace std;
 
 class TextGenerator{
 public:
-    TextGenerator(int k_input, int length_input);
+    TextGenerator(int k_input, int length_input, const string& filename);
+
+    string Generate_text();
 
     string ReturnWord();
 private:
     int k;
     int length;
+    string text_file;
     string word;
 };
+#endif
