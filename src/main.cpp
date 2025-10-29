43b628b9f60868afc38d976d7c6e60422c9ae0f3
jacobrak <155147415+jacobrak@users.noreply.github.com>
2025-10-29 16:13:21 +0100

    main cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,12 @@ int main(int argc, char* argv[]) {
         cerr << "output should be in format ./output <k> <filename> <length>" << endl;
         exit(1);
     }
+    // Parameters
     int k = stoi(argv[1]);  
     int length = stoi(argv[3]);
-    string filename =  argv[2];
+    string filename = argv[2];
+
+    // Class Call
     TextGenerator text_generation(k, length, filename);
     string output = text_generation.Generate_text();
     cout << output << endl;
