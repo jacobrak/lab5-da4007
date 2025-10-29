f6ad79e92c5bbc325d3fedec4b5e3ac149cbd2b4
jacobrak <155147415+jacobrak@users.noreply.github.com>
2025-10-29 15:37:42 +0100

    working mainfile

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include "LanguageModel.h"
+#include "TextGenerator.h"
 #include <iostream>
 
 int main() {
     int k = 3;  
-
-    LanguageModel model(k);
-    model.train("moby_dick.txt");  
-    string output = model.PickRandom();
-    cout << output;
+    int length = 6;
+    string filename =  "../moby_dick.txt";
+    TextGenerator text_generation(k, length, filename);
+    string output = text_generation.Generate_text();
+    cout << output << endl;
     return 0;
 }
