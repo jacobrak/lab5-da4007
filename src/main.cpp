8603abdc5022b8bfa7bc300b8676d15dbed40e08
jacobrak <155147415+jacobrak@users.noreply.github.com>
2025-10-29 15:46:24 +0100

    working main correct formating

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,14 @@
 #include "TextGenerator.h"
 #include <iostream>
 
-int main() {
-    int k = 3;  
-    int length = 6;
-    string filename =  "../moby_dick.txt";
+int main(int argc, char* argv[]) {
+    if (argc != 4){
+        cerr << "output should be in format ./output <k> <filename> <length>" << endl;
+        exit(1);
+    }
+    int k = stoi(argv[1]);  
+    int length = stoi(argv[3]);
+    string filename =  argv[2];
     TextGenerator text_generation(k, length, filename);
     string output = text_generation.Generate_text();
     cout << output << endl;
